1fb513303cff78758f26dbbdb5aea6886c6da2d7
funnypan <gemini0525@foxmail.com>
2020-10-28 19:56:15 +0800

    add vanilla-spa

---
+++ Vanilla-SPA/js/app.js
@@ -0,0 +1,9 @@
+(function () {
+    function init(){
+        let router=new Router([
+            new Route("home","home.html",true),
+            new Route("about","about.html")
+        ])
+    }
+    init();
+}())
